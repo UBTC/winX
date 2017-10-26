c28079f0bb9868caf64de0e8520a354b8ba28fff
wmji <maoji.wang@cims.nyu.edu>
2017-10-26 12:45:41 +0800

    add conda libs

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # winX
+
+for setting up my `WSL` in `Win10`, and the `Win10` itself.
---
+++ winx-install.sh
@@ -0,0 +1,36 @@
+echo "Installing..."
+
+conda install ipython
+conda install notebook
+conda install qtconsole
+conda install regex
+conda install setuptools
+conda install h5py
+conda install python-nmap
+conda install mpmath
+conda install sympy
+conda install jupyter
+conda install pillow
+conda install numpy
+conda install scipy
+conda install matplotlib
+conda install networkx
+conda install pandas
+conda install scikit-learn
+conda install seaborn
+conda install moviepy
+conda install future
+conda install statsmodels
+conda install mdp
+conda install ipdb
+conda install keras
+conda install flask
+conda install pygame
+conda install simpy
+conda install tushare
+conda install tensorflow
+conda install virtualenv
+conda install findspark
+conda install pymysql
+
+echo "Done"
